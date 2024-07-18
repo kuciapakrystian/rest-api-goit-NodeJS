89c07afd3d26b591d448442859d9a4b3582bb4d9
kuciapakrystian <kuciapakrystian@outlook.com>
2024-07-18 16:50:15 +0200

    updated signup method, for verification email. feat: added verification to the project. feat: added verify/:verificationToken GET path, and POST method for verify path.

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,4 +1,4 @@
-import { HttpError } from "../helpers/index.js";
+import { HttpError, sendEmail } from "../helpers/index.js";
 import { ctrlWrapper } from "../decorators/index.js";
 import User from "../models/Users.js";
 import bcrypt from "bcrypt";
@@ -8,33 +8,49 @@ import gravatar from "gravatar";
 import fs from "fs/promises";
 import path from "path";
 import Jimp from "jimp";
+import { v4 as uuid4 } from "uuid";
 
 const avatarPath = path.resolve("public", "avatars");
 
-const { JWT_SECRET } = process.env;
+const { JWT_SECRET, BASE_URL } = process.env;
 
 const signUp = async (req, res) => {
   const { password, email } = req.body;
   const user = await User.findOne({ email });
 
   if (user) {
-    throw HttpError(409, "Email is already in use");
+    throw HttpError(
+      409,
+      "Email In Use: The provided email address is already associated with an existing account. If you already have an account, please log in."
+    );
   }
   const hashPassword = await bcrypt.hash(password, 10);
   const avatarURL = gravatar.url(email, { s: "200", r: "pg", d: "mm" });
+  const verificationToken = uuid4();
+  console.log("verification code", verificationToken);
   const newUser = await User.create({
     email,
     password: hashPassword,
     avatarURL,
+    verificationToken,
   });
+
+  const verifyEmail = {
+    to: email,
+    subject: "Action Required: Verify Your Email",
+    html: `<a target="_blank" href="${BASE_URL}/api/users/verify/${verificationToken}">Click to verify email</a>`,
+  };
+
+  await sendEmail(verifyEmail);
 
   res.status(201).json({
     message:
-      "Registration successful. You can now sign in with your new account.",
+      "Congratulations! Your registration was successful. You can now sign in with your new account.",
     user: {
       email: newUser.email,
       subscription: "starter",
       avatarURL: newUser.avatarURL,
+      verificationToken,
     },
   });
 };
@@ -44,12 +60,25 @@ const signIn = async (req, res) => {
   const user = await User.findOne({ email });
 
   if (!user) {
-    throw HttpError(401, "Incorrect login or password");
+    throw HttpError(
+      401,
+      "Authentication Failed: Incorrect login or password. Please double-check your credentials and try again."
+    );
+  }
+
+  if (!user.verify) {
+    throw HttpError(
+      401,
+      "Email Not Verified: Please verify your email to access this resource. Check your inbox for instructions on how to complete the verification process."
+    );
   }
 
   const passwordCompare = await bcrypt.compare(password, user.password);
   if (!passwordCompare) {
-    throw HttpError(401, "Incorrect login or password");
+    throw HttpError(
+      401,
+      "Authentication Failed: Incorrect login or password. Please double-check your credentials and try again"
+    );
   }
 
   const { _id: id } = user;
@@ -74,7 +103,9 @@ const logOut = async (req, res) => {
   const { _id } = req.user;
   await User.findByIdAndUpdate(_id, { token: "" });
 
-  res.json({ message: "Logout successfull" });
+  res.json({
+    message: "Logout Successful: You have been successfully logged out.",
+  });
 };
 
 const updateSubscription = async (req, res) => {
@@ -87,7 +118,10 @@ const updateSubscription = async (req, res) => {
   );
 
   if (!updatedUser) {
-    throw HttpError(401, "User not found or incorrect login");
+    throw HttpError(
+      401,
+      "Authentication Failed: User not found or incorrect login. Please verify your credentials and try again."
+    );
   }
 
   res.json(updatedUser);
@@ -118,6 +152,56 @@ const updateAvatar = async (req, res) => {
   });
 };
 
+const verify = async (req, res) => {
+  const { verificationToken } = req.params;
+  const user = await User.findOne({ verificationToken });
+  if (!user) {
+    throw HttpError(
+      400,
+      "Verification Issue: The provided email was not found or has already been verified."
+    );
+  }
+
+  await User.findByIdAndUpdate(user._id, {
+    verify: true,
+    verificationToken: "",
+  });
+  res.json({
+    message:
+      "Email Verification Successful: Your email has been successfully verified. You can now access and enjoy our services. Thank you for confirming your email!",
+  });
+};
+
+const resendVerifyEmail = async (req, res) => {
+  const { email } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    throw HttpError(
+      404,
+      "User Not Found: The provided email address could not be found in our records. Please make sure you've entered the correct email or consider registering for an account."
+    );
+  }
+  if (user.verify) {
+    throw HttpError(
+      400,
+      "Email Already Verified: This email address has already been verified."
+    );
+  }
+
+  const verifyEmail = {
+    to: email,
+    subject: "Action Required: Verify Your Email",
+    html: `<a target="_blank" href="http://${BASE_URL}/api/auth/${verificationToken}">Click to verify email</a>`,
+  };
+
+  await sendEmail(verifyEmail);
+
+  res.json({
+    message:
+      "Verification Email Sent: An email with instructions to verify your email has been successfully sent. Please check your inbox and follow the provided instructions. If you don't receive the email, please check your spam folder or contact our support team for assistance.",
+  });
+};
+
 export default {
   signUp: ctrlWrapper(signUp),
   signIn: ctrlWrapper(signIn),
@@ -125,4 +209,6 @@ export default {
   logOut: ctrlWrapper(logOut),
   updateSubscription: ctrlWrapper(updateSubscription),
   updateAvatar: ctrlWrapper(updateAvatar),
+  verify: ctrlWrapper(verify),
+  resendVerifyEmail: ctrlWrapper(resendVerifyEmail),
 };
